ab7aad828fa7c3e6d091098346817dc3c76a3eef
Daniela <Daniela-Haeussler@gmx.de>
2022-11-11 11:02:40 +0100

    Mehrere leichte Funktionen

--- script_01.js
+++ script_01.js
@@ -2,3 +2,39 @@
 
 /***** Funktionen 01 *****/
 
+//1. Kapselung von Codeblöcken
+
+
+// Funktionsrumpf (body) | Callee
+// function test()
+// {
+    // console.log("Genieß deinen Tag, Hind");
+// }
+
+//Funktionsaufruf (call) 
+// test();
+
+// 2a Parametisierung + Datenübergabe von INNEN //
+
+
+
+// ausgabeName();
+
+// function ausgabeName() {
+    // let personName= prompt("put your name in here");
+    // console.log("Genieß deinen Tag," +personName);
+// };
+
+// console.log(personName);
+
+// ausgabeName(prompt("insert your name here")); //Parameter ist das Eingangstor und Argumente sind Daten für Parameter
+
+// function ausgabeName(personNames) {
+    // console.log("Genieß deinen Tag," + personNames)
+// };
+
+// ausgabeName(prompt("insert your name here"), prompt("insert your lastname here")); //Parameter ist das Eingangstor und Argumente sind Daten für Parameter
+
+// function ausgabeName(firstName, familyName) {
+    // console.log("Genieße deinen Tag," + firstName + " " + familyName);
+// };
